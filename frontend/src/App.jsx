1155bf5c4f9e1960188b00adf4d65b79a5098185
DebaPrasadbehura424 <DebaPrasadbehura424@gmail.com>
2025-03-24 22:29:30 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,17 @@ import Contact from "./pages/Contact";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
+import Navbar from "./component/Navbar";
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/contactus" element={<Contact />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contactus" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
